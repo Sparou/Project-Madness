e81d672bd24991e50ef6bdfc19cc081bf3a39d63
Nikita Semenov <nsemenov.official@gmail.com>
2023-12-05 16:47:11 +0300

    Добавьте файлы проекта.

---
+++ Assets/Sctipts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Vector2 minSceneCoordinates;
+    [SerializeField] Vector2 maxSceneCoordinates;
+    [SerializeField] float cameraSpeed = 2f; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 targetPostion = playerTransform.position;
+        Vector3 boundPosition = new Vector3 (
+            Mathf.Clamp(targetPostion.x, minSceneCoordinates.x, maxSceneCoordinates.x),
+            Mathf.Clamp(targetPostion.y, minSceneCoordinates.y, maxSceneCoordinates.y), -10);
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, cameraSpeed * Time.deltaTime);
+
+        transform.position = smoothPosition;
+        
+    }
+}
