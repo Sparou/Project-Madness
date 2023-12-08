8fb77a9b1d951a2db8dfa2dc0e4c0840081b3ea4
Maxim Dudarev <max.md8@gmail.com>
2023-12-08 17:58:03 +0300

    dialogue base

--- Assets/Sctipts/Characters/Character.cs
+++ Assets/Sctipts/Characters/Character.cs
@@ -7,6 +7,12 @@ public class Character : MonoBehaviour
     [SerializeField] string characterName;
     [SerializeField] float maxHealth;
     protected float currentHealth;
+
+    [SerializeField] private int _humanPoints
+    {
+        get { return _humanPoints; }
+        set { _humanPoints = value; }
+    }
     // Start is called before the first frame update
     void Start()
     {
