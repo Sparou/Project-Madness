46da19c3fabdc835d8a3205dfa715fe5fde230a6
gryzlovn <65680932+gryzlovn@users.noreply.github.com>
2024-04-08 02:57:45 +0300

    Level Design and Character Customization
    Redesign Village and Cave,add some colliders and triggers and customization system

--- Assets/Sctipts/PlayerMovement.cs
+++ Assets/Sctipts/PlayerMovement.cs
@@ -6,9 +6,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float moveSpeed = 2;
+    [SerializeField] Animator animator;
 
-    [SerializeField] float dashSpeed = 3;
+    [SerializeField] float dashSpeed = 4;
     [SerializeField] float dashLength = .5f;
     [SerializeField] float dashCooldown = .1f;
 
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     private float dashCooldownCounter;
     private float activeMoveSpeed;
     private Vector2 moveInput;
+    private Vector2 direction;
     private Rigidbody2D rb;
 
     private bool isDashing = false;
@@ -28,14 +30,25 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", direction.sqrMagnitude);
+        if(Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
-
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
     private void OnDash()
     {
         Debug.Log("DASH!");
